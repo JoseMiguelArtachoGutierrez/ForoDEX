11c464240807fd5077cc8c2b52df7a37c1f7e9b2
Jose Miguel <josemiguel41104@gmail.com>
2024-06-18 19:30:21 +0200

    pequeño cambio input

--- src/componentes/Perfil.jsx
+++ src/componentes/Perfil.jsx
@@ -143,7 +143,7 @@ function Perfil() {
                 <h1>{userName}</h1>
             :
             <div className='nombre'>
-                    <input type="text"  placeholder="Write your userName" value={newUserName} onChange={(e)=>{setNewUserName(e.target.value)}}  />
+                    <input type="text"  placeholder="Write your userName" value={newUserName} onChange={(e)=>{setNewUserName(e.target.value)}} maxLength='20'  />
                 <div onClick={handleSave}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512"><path d="M448 71.9c-17.3-13.4-41.5-9.3-54.1 9.1L214 344.2l-99.1-107.3c-14.6-16.6-39.1-17.4-54.7-1.8-15.6 15.5-16.4 41.6-1.7 58.1 0 0 120.4 133.6 137.7 147 17.3 13.4 41.5 9.3 54.1-9.1l206.3-301.7c12.6-18.5 8.7-44.2-8.6-57.5z" fill="#ffffff"/></svg>
                 </div>
